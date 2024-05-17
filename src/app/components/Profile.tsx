7af4b0a0789e791d00b6b72efaf850e8f20e070b
itslitulinchpin2 <itslitulinchpin@gmail.com>
2024-05-17 16:38:24 +0200

    fix : myProfile url 수정

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image'
 import React from 'react'
 import Link from 'next/link'
-import ProfileImage from './myProfile.jpg'
 export default function Profile() {
   return (
     <section className="text-center">
-      <Image priority className="mx-auto rounded-full" alt="profilePic" src={ProfileImage} width={220} height={300} ></Image>
+      <Image priority className="mx-auto rounded-full" alt="profilePic" src="/myProfile.jpg" width={220} height={300} ></Image>
       <h2 className='text-3xl font-bold mt-2'>{"Hi, I'm Seungmin"}</h2>
       <h3 className='text-xl font-semibold'>Dreaming full-stack engineer</h3>
       <p>This is my blog</p>
--- src/app/services/weather.ts
+++ src/app/services/weather.ts
@@ -2,7 +2,7 @@
 
 export async function getWeatherData(){
     const weatherKey =process.env.NEXT_PUBLIC_WEATHER_KEY
-    console.log(weatherKey);
+    // console.log(weatherKey);
     const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${weatherKey}`)
     const result = await res.json();
     return result;
