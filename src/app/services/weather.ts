55caa22d0b83b26a558e9149768d14b91811541b
itslitulinchpin2 <itslitulinchpin@gmail.com>
2024-05-19 00:28:08 +0200

    chore : 날씨데이터 렌더링주기 변경

--- src/app/services/weather.ts
+++ src/app/services/weather.ts
@@ -3,7 +3,9 @@
 export async function getWeatherData(){
     const weatherKey =process.env.NEXT_PUBLIC_WEATHER_KEY
     // console.log(weatherKey);
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${weatherKey}`)
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${weatherKey}`,{
+        next:{revalidate:60*10}
+    })
     const result = await res.json();
     return result;
 }
